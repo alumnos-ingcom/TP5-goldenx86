de85cc51061db972632a402df247309818942e74
Matías Locatti <42481638+goldenx86@users.noreply.github.com>
2021-06-13 23:20:10 -0300

    tp5ej9 added

---
+++ tp5ej9.py
@@ -0,0 +1,17 @@
+################
+# Matías Locatti - @goldenx86
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+def factorion():
+    """Imprimir factoriones"""
+    numeros = "1, 2, 145, 40585"
+    return numeros
+
+def prueba():
+    print(factorion())
+
+
+if __name__ == "__main__":
+    prueba()
